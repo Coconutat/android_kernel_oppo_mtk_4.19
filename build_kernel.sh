b66717942e02476bb275a9940574c2d9e6f07fef
Coconut <coconutat@protonmail.com>
2023-07-12 15:20:18 +0800

    Add Build Scripts

---
+++ build_kernel.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#设置环境
+
+# 交叉编译器路径
+export PATH=$PATH:/home/coconutat/github/proton-clang-master/bin
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+export ARCH=arm64
+export SUBARCH=arm64
+# export DTC_EXT=dtc
+
+if [ ! -d "out" ]; then
+	mkdir out
+fi
+
+date="$(date +%Y.%m.%d-%I:%M)"
+
+make ARCH=arm64 O=out CC=clang r6p_nodtb_defconfig
+# 定义编译线程数
+make ARCH=arm64 O=out $EV -j12 CC=clang 2>&1 | tee kernel_log-${date}.txt
+
+if [ -f out/arch/arm64/boot/Image.gz ]; then
+	echo "***Packing kernel...***"
+	cp out/arch/arm64/boot/Image.gz Image.gz
+	echo " "
+	echo "***Sucessfully built kernel...***"
+	echo " "
+	exit 0
+else
+	echo " "
+	echo "***Failed!***"
+	exit 0
+fi
